f4d02da191df7e8550e8513a1218bcfd5c178a96
Brad Robinson <contact@toptensoftware.com>
2016-10-26 10:08:06 +1100

    Tokenizer

---
+++ SimpleExpressionEngine/Tokenizer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExpressionEngine
+{
+    public class Tokenizer
+    {
+        public Tokenizer(TextReader reader)
+        {
+            _reader = reader;
+            NextChar();
+            NextToken();
+        }
+
+        TextReader _reader;
+        char _currentChar;
+        Token _currentToken;
+        double _number;
+
+        public Token Token
+        {
+            get { return _currentToken; }
+        }
+
+        public double Number
+        {
+            get { return _number; }
+        }
+
+        // Read the next character from the input strem
+        // and store it in _currentChar, or load '\0' if EOF
+        void NextChar()
+        {
+            int ch = _reader.Read();
+            _currentChar = ch < 0 ? '\0' : (char)ch;
+        }
+
+        // Read the next token from the input stream
+        public void NextToken()
+        {
+            // Skip whitespace
+            while (char.IsWhiteSpace(_currentChar))
+            {
+                NextChar();
+            }
+
+            // Special characters
+            switch (_currentChar)
+            {
+                case '\0':
+                    _currentToken = Token.EOF;
+                    return;
+
+                case '+':
+                    NextChar();
+                    _currentToken = Token.Add;
+                    return;
+
+                case '-':
+                    NextChar();
+                    _currentToken = Token.Subtract;
+                    return;
+            }
+
+            // Number?
+            if (char.IsDigit(_currentChar) || _currentChar =='.')
+            {
+                // Capture digits/decimal point
+                var sb = new StringBuilder();
+                bool haveDecimalPoint = false;
+                while (char.IsDigit(_currentChar) || (!haveDecimalPoint && _currentChar == '.'))
+                {
+                    sb.Append(_currentChar);
+                    haveDecimalPoint = _currentChar == '.';
+                    NextChar();
+                }
+
+                // Parse it
+                _number = double.Parse(sb.ToString(), CultureInfo.InvariantCulture);
+                _currentToken = Token.Number;
+                return;
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleExpressionEngine;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TokenizerTest()
+        {
+            var testString = "10 + 20 - 30.123";
+            var t = new Tokenizer(new StringReader(testString));
+
+            // "10"
+            Assert.AreEqual(t.Token, Token.Number);
+            Assert.AreEqual(t.Number, 10);
+            t.NextToken();
+
+            // "+"
+            Assert.AreEqual(t.Token, Token.Add);
+            t.NextToken();
+
+            // "20"
+            Assert.AreEqual(t.Token, Token.Number);
+            Assert.AreEqual(t.Number, 20);
+            t.NextToken();
+
+            // "-"
+            Assert.AreEqual(t.Token, Token.Subtract);
+            t.NextToken();
+
+            // "30.123"
+            Assert.AreEqual(t.Token, Token.Number);
+            Assert.AreEqual(t.Number, 30.123);
+            t.NextToken();
+        }
+    }
+}
