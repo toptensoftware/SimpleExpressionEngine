574a982f11bcc1c1afef35c5fd6ad98840dd6037
Brad Robinson <contact@toptensoftware.com>
2016-10-26 11:46:13 +1100

    Reflection Context

---
+++ SimpleExpressionEngine/ReflectionContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExpressionEngine
+{
+    public class ReflectionContext : IContext
+    {
+        public ReflectionContext(object targetObject)
+        {
+            _targetObject = targetObject;
+        }
+
+        object _targetObject;
+
+        public double ResolveVariable(string name)
+        {
+            // Find property
+            var pi = _targetObject.GetType().GetProperty(name);
+            if (pi == null)
+                throw new InvalidDataException($"Unknown variable: '{name}'");
+
+            // Call the property
+            return (double)pi.GetValue(_targetObject);
+        }
+
+        public double CallFunction(string name, double[] arguments)
+        {
+            // Find method
+            var mi = _targetObject.GetType().GetMethod(name);
+            if (mi == null)
+                throw new InvalidDataException($"Unknown function: '{name}'");
+
+            // Convert double array to object array
+            var argObjs = arguments.Select(x => (object)x).ToArray();
+
+            // Call the method
+            return (double)mi.Invoke(_targetObject, argObjs);
+        }
+    }
+}
--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -167,5 +167,43 @@ namespace UnitTests
             Assert.AreEqual(Parser.Parse("rectArea(10,20)").Eval(ctx), 200);
             Assert.AreEqual(Parser.Parse("rectPerimeter(10,20)").Eval(ctx), 60);
         }
+
+        class MyLibrary
+        {
+            public MyLibrary()
+            {
+                pi = Math.PI;
+            }
+
+            public double pi { get; private set; }
+            public double r { get; set; }
+
+            public double rectArea(double width, double height)
+            {
+                return width * height;
+            }
+
+            public double rectPerimeter(double width, double height)
+            {
+                return (width + height) * 2;
+            }
+        }
+
+        [TestMethod]
+        public void Reflection()
+        {
+            // Create a library of helper function
+            var lib = new MyLibrary();
+            lib.r = 10;
+
+            // Create a context that uses the library
+            var ctx = new ReflectionContext(lib);
+
+            // Test
+            Assert.AreEqual(Parser.Parse("rectArea(10,20)").Eval(ctx), 200);
+            Assert.AreEqual(Parser.Parse("rectPerimeter(10,20)").Eval(ctx), 60);
+            Assert.AreEqual(Parser.Parse("2 * pi * r").Eval(ctx), 2 * Math.PI * 10);
+        }
+
     }
 }
